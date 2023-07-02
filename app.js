b0481db9be914dd6cc0f4da41384673b3b10b9ce
nhatdm22022537 <92232806+nhatdm22022537@users.noreply.github.com>
2023-07-02 13:02:49 +0700

    quick fix

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+const logoImg = document.getElementById('logo-img');
+
+logoImg.addEventListener('mouseover', function() {
+  this.src = './assets/color-logo.png';
+});
+
+logoImg.addEventListener('mouseout', function() {
+  this.src = './assets/logo.png';
+});
